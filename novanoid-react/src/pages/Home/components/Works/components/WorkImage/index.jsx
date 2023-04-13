746adbbd9c0882a2816126cbe7fc77af54defbf5
khoi <hnak2442001smg4@gmail.com>
2023-04-13 20:50:24 +0700

    fix work section

--- novanoid-react/src/pages/Home/components/Works/components/WorkImage/index.jsx
+++ novanoid-react/src/pages/Home/components/Works/components/WorkImage/index.jsx
@@ -1,33 +1,67 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import './WorkImage.scss';
 
 function WorkImage(item) {
-  //   const ref = useRef(null);
-  //   console.log(item.img);
-  //   console.log('filter in WorkImage: ' + item.filter);
-  //   if (item.filter === false) {
-  //     ref.current.style.opacity = 0;
-  //   }
+  let workItem = useRef(null);
+  const workUnfilter = ' animation: fade 0.5s  forwards, zoomin 0.5s  forwards';
+  const workFilter = ' animation: fadeout 0.5s  forwards, zoomout  0.5s  forwards';
+
+  const workOverlayShow = ' animation: fadein09 0.5s forwards';
+  const workOverlayHide = 'animation: fadeout09 0.5s forwards';
+  function workoverlayOn(e) {
+    e.target.style = workOverlayShow;
+    console.log('mouseover');
+  }
+  function workoverlayOff(e) {
+    e.target.style = workOverlayHide;
+    console.log('mouseout');
+  }
+  console.log('WorkImg Filter ' + item.id + ': ' + item.filter);
+
+  const img = useRef(null);
+  const modal = useRef(null);
+  const modalImg = useRef(null);
+  const closeModal = useRef(null);
+  function modalPic(img) {
+    modal.current.style.display = 'block';
+    modalImg.current.src = img.current.src;
+    closeModal.current.onclick = function () {
+      modal.current.style.display = 'none';
+    };
+  }
+  useEffect(() => {
+    if (item.filter === true) {
+      workItem.current.style = workFilter;
+    } else {
+      workItem.current.style = workUnfilter;
+    }
+  }, [item.filter]);
   return (
-    <li className="work-item work-collection2" id="work-item-2">
-      {/* <li ref={ref} className="work-item"> */}
-      <div className="image" /*id="work-image-2"*/>
-        <div
-          //   onMouseOver={(evt) => workoverlayOn(evt)}
-          //   onMouseOut={(evt) => workoverlayOff(evt)}
-          className="overlay"
-          id="work-overlay-2"
-        >
-          <h3 id="work-title-2">TITLE</h3>
+    <>
+      <li ref={workItem} className="work-item">
+        <div className="image">
+          <div
+            onMouseOver={(e) => {
+              workoverlayOn(e);
+              console.log('onmouseover');
+            }}
+            onMouseOut={(e) => workoverlayOff(e)}
+            onClick={() => modalPic(img)}
+            className="overlay"
+            id="work-overlay-2"
+          >
+            <h3 id="work-title-2">TITLE</h3>
+          </div>
+          <img src={item.img} id="img2" alt="work-img" ref={img} />
         </div>
-        <img
-          src={item.img}
-          id="img2"
-          alt="work-img"
-          //   ref={img2}
-        />
+      </li>
+      <div id="myModal" className="modal" ref={modal}>
+        <span className="close" ref={closeModal}>
+          &times;
+        </span>
+        <img className="modal-content" id="img01" alt="modal" ref={modalImg} />
       </div>
-    </li>
+    </>
   );
 }
 
--- novanoid-react/src/pages/Home/components/Works/index.jsx
+++ novanoid-react/src/pages/Home/components/Works/index.jsx
@@ -1,328 +1,169 @@
 import './work.scss';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import '../../../../variable.scss';
 import WorkImage from './components/WorkImage';
 function Work() {
-  // let work1 = useRef(null);
-  // let work2 = useRef(null);
-  // let work3 = useRef(null);
-  // let work4 = useRef(null);
-  // let work5 = useRef(null);
-  // let work6 = useRef(null);
-  // let work7 = useRef(null);
-  // let work8 = useRef(null);
-  // let work9 = useRef(null);
-  // const workArray = [work1, work2, work3, work4, work5, work6, work7, work8, work9];
   let btnAll = useRef(null);
   let btnTravel = useRef(null);
   let btnRoad = useRef(null);
   let btnMountains = useRef(null);
   let btnNature = useRef(null);
-  let category = '';
-  // let filter = useRef(null);
   let workImage = [
     {
       id: 1,
       img: 'http://demo.rommar.in.ua/novanoid/novanoid-1/img/work-1.jpg',
-      // all: true,
-      // travel: true,
-      // road: false,
-      // mountains: false,
-      // nature: true,
-      category: 'all, travel, nature',
+      category: ' all , travel , nature ',
       filter: true,
     },
     {
       id: 2,
       img: 'http://demo.rommar.in.ua/novanoid/novanoid-1/img/work-2.jpg',
-      // all: true,
-      // travel: false,
-      // road: true,
-      // mountains: true,
-      // nature: false,
-      category: 'all, road, mountains',
+      category: ' all , road , mountains ',
+      filter: true,
     },
     {
       id: 3,
       img: 'http://demo.rommar.in.ua/novanoid/novanoid-1/img/work-3.jpg',
-      // all: true,
-      // travel: true,
-      // road: false,
-      // mountains: true,
-      // nature: false,
-      category: 'all, travel,mountains',
+      category: ' all , travel , mountains ',
+      filter: true,
     },
     {
       id: 4,
       img: 'http://demo.rommar.in.ua/novanoid/novanoid-1/img/work-4.jpg',
-      // all: true,
-      // travel: false,
-      // road: true,
-      // mountains: false,
-      // nature: true,
-      category: 'all, road, nature',
+      category: ' all , road, nature ',
+      filter: true,
     },
     {
       id: 5,
       img: 'http://demo.rommar.in.ua/novanoid/novanoid-1/img/work-5.jpg',
-      // all: true,
-      // travel: true,
-      // road: false,
-      // mountains: true,
-      // nature: false,
-      category: 'all, travel, mountains',
+      category: ' all , travel , mountains ',
+      filter: true,
     },
     {
       id: 6,
       img: 'http://demo.rommar.in.ua/novanoid/novanoid-1/img/work-6.jpg',
-      // all: true,
-      // travel: false,
-      // road: false,
-      // mountains: true,
-      // nature: true,
-      category: 'all, mountains, nature',
+      category: ' all , mountains , nature ',
+      filter: true,
     },
     {
       id: 7,
       img: 'http://demo.rommar.in.ua/novanoid/novanoid-1/img/work-7.jpg',
-      // all: true,
-      // travel: false,
-      // road: true,
-      // mountains: false,
-      // nature: true,
-      category: 'all, road, nature',
+      category: ' all , road, nature ',
+      filter: true,
     },
     {
       id: 8,
       img: 'http://demo.rommar.in.ua/novanoid/novanoid-1/img/work-8.jpg',
-      // all: true,
-      // travel: false,
-      // road: false,
-      // mountains: true,
-      // nature: true,
-      category: 'all, mountains, nature',
+      category: ' all , mountains , nature ',
+      filter: true,
     },
     {
       id: 9,
       img: 'http://demo.rommar.in.ua/novanoid/novanoid-1/img/work-9.jpg',
-      // all: true,
-      // travel: true,
-      // road: true,
-      // mountains: false,
-      // nature: false,
-      category: 'all, travel, road',
+      category: ' all , travel , road ',
+      filter: true,
     },
   ];
-  // const workUnfilter = ' animation: fade 0.5s  forwards, zoomin 0.5s  forwards';
-  // const workFilter = ' animation: fadeout 0.5s  forwards, zoomout  0.5s  forwards';
   const activeButton = 'color:white; background: var(--darker-aquamarine)';
   const inactiveButton = '';
+  const [workImgs, setWorkImg] = useState(workImage);
   function allBtn() {
-    // work1.current.style = workUnfilter;
-    // work2.current.style = workUnfilter;
-    // work3.current.style = workUnfilter;
-    // work4.current.style = workUnfilter;
-    // work5.current.style = workUnfilter;
-    // work6.current.style = workUnfilter;
-    // work7.current.style = workUnfilter;
-    // work8.current.style = workUnfilter;
-    // work9.current.style = workUnfilter;
-
     btnAll.current.style = activeButton;
     btnTravel.current.style = inactiveButton;
     btnRoad.current.style = inactiveButton;
     btnMountains.current.style = inactiveButton;
     btnNature.current.style = inactiveButton;
-
-    // category = 'all';
-    workImage[0].filter = false;
-    console.log(workImage[0].filter);
+    const nextWorkImage = workImgs.map((workImg) => {
+      if (workImg.category.includes('all') === true) {
+        return {
+          ...workImg,
+          filter: false,
+        };
+      } else {
+        return { ...workImg, filter: true };
+      }
+    });
+    setWorkImg(nextWorkImage);
   }
 
   function travelBtn() {
-    // work1.current.style = workUnfilter;
-    // work2.current.style = workFilter;
-    // work3.current.style = workUnfilter;
-    // work4.current.style = workFilter;
-    // work5.current.style = workUnfilter;
-    // work6.current.style = workFilter;
-    // work7.current.style = workFilter;
-    // work8.current.style = workFilter;
-    // work9.current.style = workUnfilter;
-
     btnTravel.current.style = activeButton;
     btnAll.current.style = inactiveButton;
-
     btnRoad.current.style = inactiveButton;
     btnMountains.current.style = inactiveButton;
     btnNature.current.style = inactiveButton;
-    // category = 'travel';
-    // console.log(category);
+    const nextWorkImage = workImgs.map((workImg) => {
+      if (workImg.category.includes('travel') === true) {
+        return {
+          ...workImg,
+          filter: false,
+        };
+      } else {
+        return { ...workImg, filter: true };
+      }
+    });
+    setWorkImg(nextWorkImage);
   }
   function roadlBtn() {
-    // work1.current.style = workFilter;
-    // work2.current.style = workUnfilter;
-    // work3.current.style = workFilter;
-    // work4.current.style = workUnfilter;
-    // work5.current.style = workFilter;
-    // work6.current.style = workFilter;
-    // work7.current.style = workUnfilter;
-    // work8.current.style = workFilter;
-    // work9.current.style = workUnfilter;
-
     btnRoad.current.style = activeButton;
     btnAll.current.style = inactiveButton;
     btnTravel.current.style = inactiveButton;
-
     btnMountains.current.style = inactiveButton;
     btnNature.current.style = inactiveButton;
-    // category = 'road';
-    // console.log(category);
+    const nextWorkImage = workImgs.map((workImg) => {
+      if (workImg.category.includes('road') === true) {
+        return {
+          ...workImg,
+          filter: false,
+        };
+      } else {
+        return { ...workImg, filter: true };
+      }
+    });
+    setWorkImg(nextWorkImage);
   }
 
   function mountainsBtn() {
-    // work1.current.style = workFilter;
-    // work2.current.style = workUnfilter;
-    // work3.current.style = workUnfilter;
-    // work4.current.style = workFilter;
-    // work5.current.style = workUnfilter;
-    // work6.current.style = workUnfilter;
-    // work7.current.style = workFilter;
-    // work8.current.style = workUnfilter;
-    // work9.current.style = workFilter;
-
     btnMountains.current.style = activeButton;
     btnAll.current.style = inactiveButton;
     btnTravel.current.style = inactiveButton;
     btnRoad.current.style = inactiveButton;
-
     btnNature.current.style = inactiveButton;
-    // category = 'mountains';
-    // console.log(category);
+    const nextWorkImage = workImgs.map((workImg) => {
+      if (workImg.category.includes('mountains') === true) {
+        return {
+          ...workImg,
+          filter: false,
+        };
+      } else {
+        return { ...workImg, filter: true };
+      }
+    });
+    setWorkImg(nextWorkImage);
   }
   function natureBtn() {
-    // work1.current.style = workUnfilter;
-    // work2.current.style = workFilter;
-    // work3.current.style = workFilter;
-    // work4.current.style = workUnfilter;
-    // work5.current.style = workFilter;
-    // work6.current.style = workUnfilter;
-    // work7.current.style = workUnfilter;
-    // work8.current.style = workUnfilter;
-    // work9.current.style = workFilter;
-
     btnNature.current.style = activeButton;
     btnAll.current.style = inactiveButton;
     btnTravel.current.style = inactiveButton;
     btnRoad.current.style = inactiveButton;
     btnMountains.current.style = inactiveButton;
-    // category = 'nature';
-    // console.log(category);
+    const nextWorkImage = workImgs.map((workImg) => {
+      if (workImg.category.includes('nature') === true) {
+        return {
+          ...workImg,
+          filter: false,
+        };
+      } else {
+        return { ...workImg, filter: true };
+      }
+    });
+    setWorkImg(nextWorkImage);
   }
 
-  // const workOverlayShow = ' animation: fadein09 0.5s forwards';
-  // const workOverlayHide = 'animation: fadeout09 0.5s forwards';
-  // function workoverlayOn(e) {
-  //   e.target.style = workOverlayShow;
-  //   console.log('mouseover');
-  // }
-  // function workoverlayOff(e) {
-  //   e.target.style = workOverlayHide;
-  //   console.log('mouseout');
-  // }
-  // const img1 = useRef(null);
-  // function modalPic(img) {
-  //   let modal = document.getElementById('myModal');
-  //   let modalImg = document.getElementById('img01');
-  //   modal.style.display = 'block';
-  //   modalImg.src = img.current.src;
-  // }
-  // function modalImage() {
-  //   var modal = document.getElementById('myModal');
-  //   var modalImg = document.getElementById('img01');
-
-  //   // var img = document.getElementById('img1');
-  //   // var overlay1 = document.getElementById('work-overlay-1');
-  //   // overlay1.onclick = function () {
-  //   //   modal.style.display = 'block';
-  //   //   modalImg.src = img.src;
-  //   // };
-
-  //   var overlay2 = document.getElementById('work-overlay-2');
-  //   var img2 = document.getElementById('img2');
-
-  //   overlay2.onclick = function () {
-  //     modal.style.display = 'block';
-  //     modalImg.src = img2.src;
-  //   };
-
-  //   var overlay3 = document.getElementById('work-overlay-3');
-  //   var img3 = document.getElementById('img3');
-
-  //   overlay3.onclick = function () {
-  //     modal.style.display = 'block';
-  //     modalImg.src = img3.src;
-  //   };
-
-  //   var overlay4 = document.getElementById('work-overlay-4');
-  //   var img4 = document.getElementById('img4');
-
-  //   overlay4.onclick = function () {
-  //     modal.style.display = 'block';
-  //     modalImg.src = img4.src;
-  //   };
-
-  //   var overlay5 = document.getElementById('work-overlay-5');
-  //   var img5 = document.getElementById('img5');
-
-  //   overlay5.onclick = function () {
-  //     modal.style.display = 'block';
-  //     modalImg.src = img5.src;
-  //   };
-
-  //   var overlay6 = document.getElementById('work-overlay-6');
-  //   var img6 = document.getElementById('img6');
-
-  //   overlay6.onclick = function () {
-  //     modal.style.display = 'block';
-  //     modalImg.src = img6.src;
-  //   };
-
-  //   var overlay7 = document.getElementById('work-overlay-7');
-  //   var img7 = document.getElementById('img7');
-
-  //   overlay7.onclick = function () {
-  //     modal.style.display = 'block';
-  //     modalImg.src = img7.src;
-  //   };
-
-  //   var overlay8 = document.getElementById('work-overlay-8');
-  //   var img8 = document.getElementById('img8');
-
-  //   overlay8.onclick = function () {
-  //     modal.style.display = 'block';
-  //     modalImg.src = img8.src;
-  //   };
-
-  //   var overlay9 = document.getElementById('work-overlay-9');
-  //   var img9 = document.getElementById('img9');
-
-  //   overlay9.onclick = function () {
-  //     modal.style.display = 'block';
-  //     modalImg.src = img9.src;
-  //   };
-  //   // Get the <span> element that closes the modal
-  //   var span = document.getElementsByClassName('close')[0];
-
-  //   // When the user clicks on <span> (x), close the modal
-  //   span.onclick = function () {
-  //     modal.style.display = 'none';
-  //   };
-  // }
   useEffect(() => {
     allBtn();
-    // modalImage();
-  });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div id="work" className="work-container">
       <h2>OUR WORKS</h2>
@@ -356,145 +197,9 @@ function Work() {
         </li>
       </ul>
       <ul className="work-collection">
-        {workImage
-          // .filter((item) => item.artist === 'John')
-          .map((item) => {
-            return <WorkImage img={item.img} filter={item.filter} key={item.id} />;
-          })}
-        {/* <li ref={work1} className="work-item work-collection1" id="work-item-1">
-          <div className="image" id="work-image-1">
-            <div
-              className="overlay"
-              id="work-overlay-1"
-              src="http://demo.rommar.in.ua/novanoid/novanoid-1/img/work-1.jpg"
-              alt="Snow"
-              onMouseOver={(evt) => workoverlayOn(evt)}
-              onMouseOut={(evt) => workoverlayOff(evt)}
-              onClick={() => modalPic(img1)}
-            >
-              <h3 id="work-title-1">TITLE</h3>
-            </div>
-            <img
-              src="http://demo.rommar.in.ua/novanoid/novanoid-1/img/work-1.jpg"
-              id="img1"
-              alt="work-img"
-              ref={img1}
-            />
-          </div>
-        </li>
-        <div id="myModal" className="modal">
-          <span className="close">&times;</span>
-          <img className="modal-content" id="img01" alt="modal" />
-        </div>
-        <li ref={work2} className="work-item work-collection2" id="work-item-2">
-          <div className="image" id="work-image-2">
-            <div
-              onMouseOver={(evt) => workoverlayOn(evt)}
-              onMouseOut={(evt) => workoverlayOff(evt)}
-              className="overlay"
-              id="work-overlay-2"
-            >
-              <h3 id="work-title-2">TITLE</h3>
-            </div>
-            <img
-              src="http://demo.rommar.in.ua/novanoid/novanoid-1/img/work-2.jpg"
-              id="img2"
-              alt="work-img"
-              // ref={img2}
-            />
-          </div>
-        </li>
-        <li ref={work3} className="work-item work-collection3" id="work-item-3">
-          <div className="image" id="work-image-3">
-            <div
-              onMouseOver={(evt) => workoverlayOn(evt)}
-              onMouseOut={(evt) => workoverlayOff(evt)}
-              className="overlay"
-              id="work-overlay-3"
-            >
-              <h3 id="work-title-3">TITLE</h3>
-            </div>
-            <img src="http://demo.rommar.in.ua/novanoid/novanoid-1/img/work-3.jpg" id="img3" alt="work-img" />
-          </div>
-        </li>
-        <li ref={work4} className="work-item work-collection4" id="work-item-4">
-          <div className="image" id="work-image-4">
-            <div
-              onMouseOver={(evt) => workoverlayOn(evt)}
-              onMouseOut={(evt) => workoverlayOff(evt)}
-              className="overlay"
-              id="work-overlay-4"
-            >
-              <h3 id="work-title-4">TITLE</h3>
-            </div>
-            <img src="http://demo.rommar.in.ua/novanoid/novanoid-1/img/work-4.jpg" id="img4" alt="work-img" />
-          </div>
-        </li>
-        <li ref={work5} className="work-item work-collection5" id="work-item-5">
-          <div className="image" id="work-image-5">
-            <div
-              onMouseOver={(evt) => workoverlayOn(evt)}
-              onMouseOut={(evt) => workoverlayOff(evt)}
-              className="overlay"
-              id="work-overlay-5"
-            >
-              <h3 id="work-title-5">TITLE</h3>
-            </div>
-            <img src="http://demo.rommar.in.ua/novanoid/novanoid-1/img/work-5.jpg" id="img5" alt="work-img" />
-          </div>
-        </li>
-        <li ref={work6} className="work-item work-collection6" id="work-item-6">
-          <div className="image" id="work-image-6">
-            <div
-              onMouseOver={(evt) => workoverlayOn(evt)}
-              onMouseOut={(evt) => workoverlayOff(evt)}
-              className="overlay"
-              id="work-overlay-6"
-            >
-              <h3 id="work-title-6">TITLE</h3>
-            </div>
-            <img src="http://demo.rommar.in.ua/novanoid/novanoid-1/img/work-6.jpg" id="img6" alt="work-img" />
-          </div>
-        </li>
-        <li ref={work7} className="work-item work-collection7" id="work-item-7">
-          <div className="image" id="work-image-7">
-            <div
-              onMouseOver={(evt) => workoverlayOn(evt)}
-              onMouseOut={(evt) => workoverlayOff(evt)}
-              className="overlay"
-              id="work-overlay-7"
-            >
-              <h3 id="work-title-7">TITLE</h3>
-            </div>
-            <img src="http://demo.rommar.in.ua/novanoid/novanoid-1/img/work-7.jpg" id="img7" alt="work-img" />
-          </div>
-        </li>
-        <li ref={work8} className="work-item work-collection8" id="work-item-8">
-          <div className="image" id="work-image-8">
-            <div
-              onMouseOver={(evt) => workoverlayOn(evt)}
-              onMouseOut={(evt) => workoverlayOff(evt)}
-              className="overlay"
-              id="work-overlay-8"
-            >
-              <h3 id="work-title-8">TITLE</h3>
-            </div>
-            <img src="http://demo.rommar.in.ua/novanoid/novanoid-1/img/work-8.jpg" id="img8" alt="work-img" />
-          </div>
-        </li>
-        <li ref={work9} className="work-item work-collection9" id="work-item-9">
-          <div className="image" id="work-image-9">
-            <div
-              onMouseOver={(evt) => workoverlayOn(evt)}
-              onMouseOut={(evt) => workoverlayOff(evt)}
-              className="overlay"
-              id="work-overlay-9"
-            >
-              <h3 id="work-title-9">TITLE</h3>
-            </div>
-            <img src="http://demo.rommar.in.ua/novanoid/novanoid-1/img/work-9.jpg" id="img9" alt="work-img" />
-          </div>
-        </li> */}
+        {workImgs.map((item) => {
+          return <WorkImage img={item.img} filter={item.filter} id={item.id} key={item.id} />;
+        })}
       </ul>
     </div>
   );
